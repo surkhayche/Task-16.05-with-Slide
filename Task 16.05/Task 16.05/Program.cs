20c4762ec93b385cc805e137e0f314c82c40e04c
Surkhay <surkhayshikhiyev@gmail.com>
2024-05-16 03:06:43 +0400

    Task with slide

---
+++ Task 16.05/Task 16.05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_16._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string word = "Code Academy";
+            //int[] letterNumbers = new int[26];
+
+            //foreach (char letter in word)
+            //{
+            //    if (char.IsLetter(letter))
+            //    {
+            //        int index = letter - 'a';
+            //        letterNumbers[index]++;
+            //    }
+            //}
+            //for (int i = 0; i < 26; i++)
+            //{
+            //    char letter = (char)('a' + i);
+            //    if (letterNumbers[i] > 0)
+            //    {
+            //        Console.WriteLine($"{letter} herfi {letterNumbers} defe islenib");
+            //    }
+            //}
+
+            
+        }
+    }
+}
